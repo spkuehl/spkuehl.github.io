cb80d1a1650dc7b842e6734aba4296c5ef5b3eed
spkuehl <skuehlhorn@gmail.com>
2020-05-24 14:35:56 -0500

    Initial commit with gallery grid.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+  html { font-size: 22px; }
+  body { font-family: 'Roboto', sans-serif; }
+  a {
+    color: #0094FF;
+    text-decoration: none;
+  }
+  a:hover { color: #0074C6; }
 </style>
